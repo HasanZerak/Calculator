71a2efceaf50674d160e5f77967a1292d7090975
Zauq <zerakhasanhashmi@gmail.com>
2022-07-05 16:03:58 +0530

    Add only 1 decimal per input, round to 5 decimal places

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let buttonNumber = document.querySelectorAll('.number');           //number butt
 let buttonOperator = document.querySelectorAll('.operator');           //operator button pressed
 let buttonEquals = document.querySelector('.equals');           //equals button pressed
 let buttonClear = document.querySelector('.clear');         //clear button
+let buttonDecimal = document.querySelector('.decimal');         //decimal button
 let displayValue = '';           //number that appears on the screen when pressing buttons
 let display = document.querySelector('.display');
 let firstInput = 0;
 let secondInput = 0;
 let operatorInput = '';
-let displayOperator
+let displayOperator;
+let decimalCheck = 0;
 let output;
 
 function add(num1, num2) {           //addition function
@@ -30,20 +32,26 @@ function operate(operator, num1, num2) {         //function to take input number
     switch (operator) {
         case '+':           //add
             output = add(num1, num2);
-            display.textContent = +output.toFixed(15);
+            display.textContent = +output.toFixed(5);
             return output;
         case '-':           //subtract
             output = sub(num1, num2);
-            display.textContent = +output.toFixed(15);
+            display.textContent = +output.toFixed(5);
             return output;
         case '*':           //multiply
             output = prod(num1, num2);
-            display.textContent = +output.toFixed(15);
+            display.textContent = +output.toFixed(5);
             return +output;
         case '/':           //divide
             output = div(num1, num2);
-            display.textContent = +output.toFixed(15);
-            return output;
+            if (output === Infinity) {
+                display.textContent = "ERROR";
+            }
+            else {
+                display.textContent = +output.toFixed(5);
+                return output;
+            }
+            break;
         default:
             alert("invalid operator");
     }
@@ -64,8 +72,26 @@ buttonNumber.forEach(buttonOne => {         //event lisetener to catch and store
     });
 });
 
+buttonDecimal.addEventListener("click", function () {
+    if (decimalCheck === 0) {
+        if (operatorInput === '') {           //checking if this is the first input
+            displayValue += this.textContent;           //add new number to the end of the list
+            display.textContent = displayValue;         //change display according to the numbers pressed 
+            firstInput = displayValue;          //storing the value of first input
+            decimalCheck = 1;
+        }
+        else {
+            displayValue += this.textContent;           //add new number to the end of the list
+            display.textContent = displayValue;         //change display according to the numbers pressed 
+            secondInput = displayValue;            //storing the value of second input
+            decimalCheck = 1;
+        }
+    }
+});
+
 buttonOperator.forEach(buttonOne => {           //event lisetener to catch and store each operator pressed
     buttonOne.addEventListener("click", function () {
+        decimalCheck = 0;
         if (firstInput != "" && secondInput != "") {         //if the first and second input have been entered once
             firstInput = +operate(operatorInput, firstInput, secondInput);          //new value of first input after second operator has been pressed
         }
@@ -80,8 +106,8 @@ buttonEquals.addEventListener("click", function () {          //function to find
     operate(operatorInput, firstInput, secondInput);
 });
 
-buttonClear.addEventListener("click", function(){
-    displayValue = 0;
+buttonClear.addEventListener("click", function () {
+    displayValue = '';
     display.textContent = displayValue;
     firstInput = '';
     secondInput = '';
